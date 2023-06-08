9c0d01d9a8e60f5b910416cd6939ab323fba81e8
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2023-06-08 20:20:06 +0700

    checkfee.ts

--- scripts/checkfee.ts
+++ scripts/checkfee.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const protocolFeesCollector = "0xCd16000Da4D7A670AD2e03988E5cFf";
+  const protocolFeesCollector = "0xCd16000Da4D7A670AD2e03988E5cFfD11C34b6eE";
   const ProtocolFeesCollector = await ethers.getContractAt(
     "IProtocolFeesCollector",
     protocolFeesCollector
